da0fd3cfe94d033dc2de6dbdff94ed5323657c02
nrxo777 <noorxcalma@gmail.com>
2025-02-18 23:41:25 +0600

    vite_y

---
+++ vite_y/src/App.jsx
@@ -0,0 +1,82 @@
+import { useCallback, useState } from "react";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [number, setNumber] = useState(false);
+  const [symbol, setSymbol] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const password_generator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (number) str += "0123456789";
+    if (symbol) str += "!@#$%^&*()_+=-`~;:";
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, number, symbol, setPassword]);
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-4 my-8  bg-gray-700 text-orange-500">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3 bg-white text-black"
+            placeholder="Password"
+            readOnly
+          />
+          <button className="outline-none bg-teal-700 text-white px-3 py-0.5 shrink-0">
+            COPY
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={7}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label htmlFor="">Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={number}
+              id="numberInput"
+              onChange={() => {
+                setNumber((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={symbol}
+              id="symbolInput"
+              onChange={() => {
+                setSymbol((prev) => !prev);
+              }}
+            />
+            <label htmlFor="symbolInput">Symbols</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;

c2d6e0622a8930d796deb2140b2af852ad61b39c
nrxo777 <noorxcalma@gmail.com>
2025-02-21 18:49:41 +0600

    github_update

--- vite_z/components/Github/Github.jsx
+++ vite_z/components/Github/Github.jsx
@@ -16,9 +16,34 @@ const Github = () => {
   }, []);
 
   return (
-    <div className="text-center m4 bg-gray-600 text-white p-4 text-3xl">
-      <h2>Github Followers: {data.followers}</h2>
-      <img className="" src={data.avatar_url} alt="user-image" width={300} />
+    <div className="bg-gray-600 text-white">
+      <h2 className="text-center m-4  p-4 text-3xl">
+        Github Name: {data.name}
+      </h2>
+      <div className="flex flex-col items-center text-white">
+        <img
+          className="rounded-full w-96 h-96 m3 px-7 py-7"
+          src={data.avatar_url}
+          alt="user-image"
+          width={300}
+        />
+        <div className="mb-2 text-2xl px-7 py-7 text-left tracking-tight text-gray-50">
+          <h3>
+            User ID:{" "}
+            <span className="font-bold text-orange-400">{data.login}</span>
+          </h3>
+
+          <h3>
+            User following:{" "}
+            <span className="font-bold text-orange-400">{data.following}</span>
+          </h3>
+
+          <h3>
+            User followers:{" "}
+            <span className="font-bold text-orange-400">{data.followers}</span>
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };

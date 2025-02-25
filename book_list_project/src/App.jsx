d18199e93c5a3a9fb245e696410fc4fa79c41d07
nrxo777 <noorxcalma@gmail.com>
2025-02-25 23:22:52 +0600

    cleaning_up_book

--- book_list_project/src/App.jsx
+++ book_list_project/src/App.jsx
@@ -1,26 +1,14 @@
-import Book from "./Book/Book";
+import Book from "./Book/Book.jsx";
 
-function App() {
-  let books = [
-    { book_name: "Make Out", writer: "Jiraya" },
-    { book_name: "Shoe Dog", writer: "Phill Knight" },
-    { book_name: "Naruto", writer: "Kishimoto" },
-    { book_name: "One Piece", writer: "Oda" },
-    { book_name: "Bleach", writer: "Kubo" },
-  ];
-
-  const books_state = books.map((book) => {
-    return <Book book_name={book.book_name} writer={book.writer} />;
-  });
-
+const App = () => {
   return (
     <>
       <h1 className="text-3xl text-center m-3 p-3 font-bold underline">
         Book List Project
       </h1>
-      <div>{books_state}</div>
+      <Book />
     </>
   );
-}
+};
 
 export default App;
--- book_list_project/src/Book/Book.jsx
+++ book_list_project/src/Book/Book.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 
 const Book = (props) => {
-  return (
-    <>
-      <div className="border rounded-xl border-orange-500 text-center m-10 mx-50 p-4 text-xl ">
-        <h3>
-          <span className="text-orange-300">Book Name:</span> {props.book_name}
-        </h3>
-        <h4>
-          <span className="text-orange-300">Writer:</span> {props.writer}
-        </h4>
-      </div>
-    </>
-  );
-};
+  let books = [
+    { book_name: "Make Out", writer: "Jiraya" },
+    { book_name: "Shoe Dog", writer: "Phill Knight" },
+    { book_name: "Naruto", writer: "Kishimoto" },
+    { book_name: "One Piece", writer: "Oda" },
+    { book_name: "Bleach", writer: "Kubo" },
+  ];
 
+  const books_state = books.map((book) => {
+    return (
+      <>
+        <div
+          key={book.book_name}
+          className="border rounded-xl border-orange-500 text-center m-10 mx-50 p-4 text-xl "
+        >
+          <h3>
+            <span className="text-orange-300">Book Name: {book.book_name}</span>
+          </h3>
+          <h4>
+            <span className="text-orange-300">Writer: {book.writer}</span>
+          </h4>
+        </div>
+      </>
+    );
+  });
+  return <div>{books_state}</div>;
+};
 export default Book;

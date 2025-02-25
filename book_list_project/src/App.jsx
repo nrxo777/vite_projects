2f6b91a0b2dc3591d74b3afecf48a29ff4a05d6a
nrxo777 <noorxcalma@gmail.com>
2025-02-25 21:53:04 +0600

    book_list_project

---
+++ book_list_project/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl text-center m-3 p-3 font-bold underline">
+        Book List Project
+      </h1>
+    </>
+  );
+}
+
+export default App;
